54761ce0107a36e002568c6a08ef663a0d293738
SuperBeppe98 <giuseppebrusco98@gmail.com>
2023-03-06 11:41:02 +0100

    fix

--- checker.py
+++ checker.py
@@ -21,7 +21,7 @@ data_scadenza_str = input(
 data_scadenza = datetime.datetime.strptime(data_scadenza_str, '%d-%m-%Y')
 
 # Load the Excel file
-df = pd.read_excel("prodotti.xlsx")
+df = pd.read_excel("products.xlsx")
 
 # Rename the "Part Name" column to "Product Name" and move it to position 0, left-aligning the text
 df = df.rename(columns={"Part Name": "Product Name"})
@@ -35,14 +35,14 @@ df = df.rename(columns={"Quantity": "Quantity"})
 df = df.rename(columns={"purchase_price": "Purchase Price"})
 
 # Convert the "Packaging" column to a datetime object
-df["Packaging"] = pd.to_datetime(df["Packaging"], format="%m/%y")
+df["Confezionamento"] = pd.to_datetime(df["Confezionamento"], format="%m/%y")
 
 # Find products with an expiration date up to the end of 2023
-mask = (df["Packaging"] <= pd.Timestamp(data_scadenza))
+mask = (df["Confezionamento"] <= pd.Timestamp(data_scadenza))
 expiring_products = df.loc[mask]
 
 # Convert the "Packaging" column to a string in the format "dd-mm-yyyy"
-expiring_products["Packaging"] = expiring_products["Packaging"].dt.strftime(
+expiring_products["Confezionamento"] = expiring_products["Confezionamento"].dt.strftime(
     '%d-%m-%Y')
 
 # Print the expiring products without the row number
